6b26072ab6ada2f04d87425587b9c52e86517191
kwkwok1980 <kwkwok1980@gmail.com>
2019-05-06 00:34:27 +0800

    Update goldman_number_subarrays_product_less_k.cpp

--- goldman_number_subarrays_product_less_k.cpp
+++ goldman_number_subarrays_product_less_k.cpp
@@ -1,1 +1,58 @@
+//https://www.geeksforgeeks.org/number-subarrays-product-less-k/
 
+#include <iostream>
+#include <array>
+
+int Solve(const int* values, int N, int K)
+{
+    int result = 1;
+    int count = 0;
+    int n = 0;
+
+    for (int i=0; i<N; ++i)
+    {
+        for (int j=n; j<N; ++j)
+        {
+            if (result * values[j] > K)
+            {
+                break;
+            }
+
+            result = result * values[j];
+            ++n;
+        }
+
+        if (n>i)
+        {
+            count += (n-i);
+            result = result / values[i];
+        }
+        else
+        {
+            result = 1;
+            n = i+1;
+        }
+    }
+
+    return count;
+}
+
+
+int main()
+{
+    {
+        std::array<int, 4> values {1, 2, 3, 4};
+        int K = 10;
+        int result = Solve(values.data(), values.size(), K);
+        std::cout << result << std::endl;
+    }
+
+    {
+        std::array<int, 7> values {1, 9, 2, 8, 6, 4, 3};
+        int K = 100;
+        int result = Solve(values.data(), values.size(), K);
+        std::cout << result << std::endl;
+    }
+
+    return 0;
+}
