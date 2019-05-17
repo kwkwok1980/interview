735131a67d61eeda8edd5769532d7f9773d3d327
kwkwok1980 <kwkwok1980@gmail.com>
2019-05-17 23:42:43 +0800

    Create goldman-minimum-cost-of-ropes.cpp

---
+++ goldman-minimum-cost-of-ropes.cpp
@@ -0,0 +1,58 @@
+//https://practice.geeksforgeeks.org/problems/minimum-cost-of-ropes/0
+
+#include <stack>
+#include <iostream>
+#include <vector>
+#include <queue>
+
+void Solve(const std::vector<std::int64_t>& values)
+{
+    std::priority_queue<std::int64_t, std::vector<std::int64_t>, std::greater<std::int64_t>> queues{};
+    for(auto value : values)
+    {
+        queues.push(value);
+    }
+    
+    std::int64_t cost = 0;
+    while(true)
+    {
+        std::int64_t min1 = queues.top();
+        queues.pop();
+
+        std::int64_t min2 = queues.top();
+        queues.pop();
+
+        std::int64_t min3 = min1 + min2;
+        //std::cout << min1 << " " << min2 << " " << min3 << std::endl;
+        cost += min3;
+
+        if (queues.empty())
+        {
+            break;
+        }
+        else
+        {
+            queues.push(min3);
+        }
+    }
+
+    std::cout << cost << std::endl;
+}
+
+int main() {
+
+    std::int64_t T, N;
+    std::cin >> T;
+    for (std::int64_t t=0; t<T; ++t)
+    {
+        std::cin >> N;
+        std::vector<std::int64_t> values{};
+        for (std::int64_t n=0; n<N; ++n)
+        {
+            std::int64_t i ;
+            std::cin >> i;
+            values.push_back(i);
+        }
+        Solve(values);
+    }
+}
