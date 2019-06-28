bc2dab9095c335a7140cee455c1723b38ce0edae
kwkwok1980 <kwkwok1980@gmail.com>
2019-06-28 21:24:35 +0800

    Create gs-squares-in-nn-chessboard.cpp

---
+++ gs-squares-in-nn-chessboard.cpp
@@ -0,0 +1,40 @@
+//https://practice.geeksforgeeks.org/problems/squares-in-nn-chessboard/0
+#include <iostream>
+
+void Solve(int N)
+{
+    std::int64_t count=0;
+    std::int64_t prev=0;
+    
+    if (N==1)
+    {
+        std::cout << 1 << std::endl;
+        return;
+    }
+    
+    prev = 1;
+    for (int n=2; n<=N; ++n)
+    {
+        count = prev;
+        for (int m=1; m<=n; ++m)
+        {
+            count += 2*m;
+        }
+        count -= n;
+        prev = count;
+    }
+    
+    std::cout << count << std::endl;
+}
+
+int main() {
+	int T=0;
+	int N=0;
+	std::cin >> T;
+	for (int t=0; t<T; ++t)
+	{
+	    std::cin >> N;
+	    Solve(N);
+	}
+	return 0;
+}
