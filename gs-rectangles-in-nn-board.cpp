29bfc3cba5c7552e66cd16ce402bfaac97d5dfcd
kwkwok1980 <kwkwok1980@gmail.com>
2019-07-03 22:36:53 +0800

    Create gs-rectangles-in-nn-board.cpp

---
+++ gs-rectangles-in-nn-board.cpp
@@ -0,0 +1,42 @@
+//https://practice.geeksforgeeks.org/problems/rectangles-in-nn-board/0
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <tuple>
+#include <set>
+#include <memory>
+#include <unordered_map>
+#include <unordered_set>
+#include <sstream>
+#include <fstream>
+#include <queue>
+#include <stack>
+#include <cmath>
+
+#include <iostream>
+
+void Solve(int N)
+{
+    std::int64_t sum = 1;
+    std::int64_t count = 0;
+    for (int i=2; i<=N; ++i)
+    {
+        count = count + i * sum;
+        sum = sum + i;
+    }
+    std::cout << count * 2 << std::endl;
+}
+
+int main() {
+    int T = 0;
+    int N = 0;
+    std::cin >> T;
+    for (int t=0; t<T; ++t)
+    {
+        std::cin >> N;
+        Solve(N);
+    }
+    return 0;
+}
