b7c3ee994ef3e10a8070ae429212fbda33b3c124
kwkwok1980 <kwkwok1980@gmail.com>
2025-05-22 00:58:03 +0800

    Create gs-nodes-at-given-distance-in-binary-tree.cpp

---
+++ gs-nodes-at-given-distance-in-binary-tree.cpp
@@ -0,0 +1,91 @@
+// https://www.geeksforgeeks.org/problems/nodes-at-given-distance-in-binary-tree/1?page=4&company=Goldman%20Sachs&sortBy=difficulty
+
+/* A binary Tree node
+struct Node
+{
+    int data;
+    struct Node *left, *right;
+};
+*/
+
+struct Path {
+    int data = -1;
+    int parent = -1;
+    int left = -1;
+    int right = -1;
+    bool visited = false;
+    int distance = 0;
+};
+
+class Solution {
+
+  public:
+
+    std::vector<int> KDistanceNodes(Node* root, int target, int k) {
+        std::deque<Node*> nodeList{};
+        std::unordered_map<int, Path*> pathMap{};
+        Path* path;
+        Path* left;
+        Path* right;
+        Path* parent;
+        Node* node;
+        path = pathMap[root->data] = new Path{};
+        path->data = root->data;
+        nodeList.emplace_back(root);
+        while (!nodeList.empty()) {
+            node = nodeList.front();
+            path = pathMap[node->data];
+            if (node->left) {
+                path->left = node->left->data;
+                left = pathMap[node->left->data] = new Path{};
+                left->data = node->left->data;
+                left->parent = node->data;
+                nodeList.emplace_back(node->left);
+            }
+            if (node->right) {
+                path->right = node->right->data;
+                right = pathMap[node->right->data] = new Path{};
+                right->data = node->right->data;
+                right->parent = node->data;
+                nodeList.emplace_back(node->right);
+            }
+            nodeList.pop_front();
+        }
+        std::vector<int> results{};
+        std::deque<Path*> distanceList{};
+        distanceList.push_back(pathMap[target]);
+        while (!distanceList.empty()) {
+            path = distanceList.front();
+            path->visited = true;
+            if (path->distance < k) {
+                if (path->parent != -1) {
+                    parent = pathMap[path->parent];
+                    if (!parent->visited) {
+                        parent->distance = path->distance + 1;
+                        distanceList.push_back(parent);
+                    }
+                }
+                if (path->left != -1) {
+                    left = pathMap[path->left];
+                    if (!left->visited) {
+                        left->distance = path->distance + 1;
+                        distanceList.push_back(left);
+                    }
+                }
+                if (path->right != -1) {
+                    right = pathMap[path->right];
+                    if (!right->visited) {
+                        right->distance = path->distance + 1;
+                        distanceList.push_back(right);
+                    }
+                }
+            } else {
+                results.push_back(path->data);
+            }
+            distanceList.pop_front();
+        }
+        std::sort(results.begin(), results.end());
+        return results;
+    }
+};
+
